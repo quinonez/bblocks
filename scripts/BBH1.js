23b9101cf86db0b11e9d224cd17bfa57e65b1d3c
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-09-27 14:32:19 -0500

    Redefinition of Module in order to get a constructor function that receives arguments.

--- scripts/BBH1.js
+++ scripts/BBH1.js
@@ -7,54 +7,56 @@ Module Dependencies: d3, THREE, BBMathematics, BBStatistics.
  
 */
 define( [ 'd3', 'THREE' ], function( d3, THREE ){
-
+  "use strict";
 
   // Object Constructor BBH1 Building Block Histogram 1D. 
-  var BBH1 = function( name, title, nbinsx, xmin, xmax ){
-    var fDimension = 1; // Dimension of the plot data.
-    var name = name;
-    var title = title;
-    var nbinsx = nbinsx;
-    var xmin = xmin;
-    var xmax = xmax;
-    var rawData = []; // data array filled by Fill function.
-    var freqData = new Array( nbinsx + 2 ); // data array of frequencies for each bin. Two bins extra have been added, one for underflow and the another one for overflow.
+  function BBH1( name, title, nbinsx, xmin, xmax ){
+    this.fDimension = 1; // Dimension of the plot data.
+    this.name = name;
+    this.title = title;
+    this.nbinsx = nbinsx;
+    this.xmin = xmin;
+    this.xmax = xmax;
+    this.rawData = []; // data array filled by Fill function.
+    this.freqData = new Array( nbinsx + 2 ); // data array of frequencies for each bin. Two bins extra have been added, one for underflow and the another one for overflow.
+  }
 
-    function Fill( value ) {
-      return rawData.push( value );
+
+  BBH1.prototype = {
+    constructor: BBH1,
+
+    Fill: function( value ) {
+      return this.rawData.push( value );
+    },
+    
+    Draw: function(){
+    var binxlow = this.xmin;
+    var binwidth = ( this.xmax - this.xmin ) / this.nbinsx;
+    var binxup = binxlow + binwidth;
+
+    // Initialize frequencies to zero.
+    for( var i = 0; i < this.freqData.length; i++ ){
+      this.freqData[ i ] = 0;
     }
- 
-    function Draw(){
-      var binxlow = xmin;
-      var binwidth = ( xmax - xmin ) / nbinsx;
-      var binxup = binxlow + binwidth;
 
-      // Initialize frequencies to zero.
-      for( var i = 0; i < freqData.length; i++ ){
-        freqData[ i ] = 0;
-      }
-
-      // First fill array of frequencies for each bin: freqData.
-      for( var i = 0; i < rawData.length; i++ ){
-        if( rawData[ i ] < xmin ) freqData[ 0 ] += 1; // Underflow.
-        if( rawData[ i ] >= xmax ) freqData[ nbinsx + 1 ] += 1; // Overflow.
-        // From index 1 til index nbinsx-2 of freqData Array.
-        for( var j = 1; j < freqData.length - 1; j++ ){
-          if( rawData[ i ] >= binxlow && rawData[ i ] < binxup ){
-            freqData[ j ] += 1;
-            break;
-	  }
+    // First fill array of frequencies for each bin: freqData.
+    for( var i = 0; i < this.rawData.length; i++ ){
+      if( this.rawData[ i ] < this.xmin ) this.freqData[ 0 ] += 1; // Underflow.
+      if( this.rawData[ i ] >= this.xmax ) this.freqData[ nbinsx + 1 ] += 1; // Overflow.
+      // From index 1 til index nbinsx-2 of freqData Array.
+      for( var j = 1; j < this.freqData.length - 1; j++ ){
+        if( this.rawData[ i ] >= binxlow && this.rawData[ i ] < binxup ){
+          this.freqData[ j ] += 1;
+          break;
         }
-        // Go to the next bin
-        binxlow = binxup;
-        binxup += binwidth;   
       }
-
-
+      // Go to the next bin
+      binxlow = binxup;
+      binxup += binwidth;   
+    }
       // **************************************************************
       // Now start visualization
       // **************************************************************
-
       var margin = { top:20, right:40, bottom: 30, left:20 };
       var width = 640 -  margin.left - margin.right ;
       var height = 400 -  margin.top - margin.bottom ;
@@ -68,8 +70,8 @@ define( [ 'd3', 'THREE' ], function( d3, THREE ){
       var xScale = d3.scale.linear().range( [ 0, width ] );
       var yScale = d3.scale.linear().range( [ height, 0 ] );
       // Defining the plot's domain
-      xScale.domain( xmin, xmax ).nice();
-      yScale.domain( 0, d3.max( rawData ) ).nice();
+      xScale.domain( this.xmin, this.xmax ).nice();
+      yScale.domain( 0, d3.max( this.rawData ) ).nice();
 
       var xAxis = d3.svg.axis()
         .scale( xScale )
@@ -92,21 +94,17 @@ define( [ 'd3', 'THREE' ], function( d3, THREE ){
 
       // Displaying data    
       freqData.forEach(
-        chart2.append( "rect" )
+        chart.append( "rect" )
           .attr( "x", xScale )
           .attr( "width", binwidth )
           .attr( "y", yScale )
           .attr( "height", height - yScale )
       );
     } // Ends function Draw
-    return {
-      Fill: Fill,
-      Draw: Draw
-    };
-  }; // Ends function BBH1
-  return {
-    BBH1: BBH1
   };
-}); // Ends Module BBH
+
+  return BBH1;
+ 
+}); // Ends Module BBH1
 
 
